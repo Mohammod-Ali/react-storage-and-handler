402c20da190a97714b444c3c35241181c4aad73c
Mohammod Ali <mohammodali651@gmail.com>
2022-09-24 02:44:23 +0600

    store data in an object with local storage

--- src/components/Cosmotic/Cosmetic.js
+++ src/components/Cosmotic/Cosmetic.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
-
+    
     const addToCart = (id) =>{
-        console.log(id);
+        addToDb(id)
     }
 
     return (
